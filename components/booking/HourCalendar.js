8c7d1fddaacf8402b0c640665d1e017874781105
Dawid Spisak <dawidspisak@gmail.com>
2022-02-05 17:54:45 +0100

    add more styles

--- components/booking/HourCalendar.js
+++ components/booking/HourCalendar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import { useRouter } from 'next/router'
 import HourTile from './HourTile'
 
 const HourCalendar = () => {
   const router = useRouter()
+  const [selected, setSelected] = useState(null)
+  const handleClick = (e) => {
+    setSelected(e.target.textContent)
+  }
+  console.log(selected)
+
   return (
     <div className="text-evenly flex h-full w-full flex-col items-center justify-evenly py-4">
       <div className="relative h-[10%] w-full text-center">
@@ -15,15 +21,60 @@ const HourCalendar = () => {
         <h1 className="text-2xl">Wybierz godzinę</h1>
       </div>
       <div className="flex max-h-[80%] w-full flex-col items-center  space-y-2 overflow-x-hidden overflow-y-scroll scrollbar-hide">
-        <HourTile hour="11:00" left="0" />
-        <HourTile hour="12:00" left="1" />
-        <HourTile hour="13:00" left="4" />
-        <HourTile hour="14:00" left="7" />
-        <HourTile hour="15:00" left="11" />
-        <HourTile hour="16:00" left="16" />
-        <HourTile hour="17:00" left="17" />
-        <HourTile hour="18:00" left="14" />
-        <HourTile hour="19:00" left="19" />
+        <HourTile
+          hour="11:00"
+          left="0"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="12:00"
+          left="1"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="13:00"
+          left="4"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="14:00"
+          left="7"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="15:00"
+          left="11"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="16:00"
+          left="16"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="17:00"
+          left="17"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="18:00"
+          left="14"
+          onClick={handleClick}
+          selected={selected}
+        />
+        <HourTile
+          hour="19:00"
+          left="19"
+          onClick={handleClick}
+          selected={selected}
+        />
         <HourTile hour="20:00" left="20" full={true} />
       </div>
       <button className="h-10 rounded border border-solid border-stone-600 px-8 py-1">
--- components/booking/HourTile.js
+++ components/booking/HourTile.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const HourTile = ({ left, hour, full }) => {
+const HourTile = ({ left, hour, full, onClick, selected }) => {
   return (
     <div
-      className={`flex h-16 min-h-[64px] w-4/5 cursor-pointer items-center rounded-lg border-2 border-l-4 border-l-red-600 ${
+      className={`flex h-16 min-h-[64px] w-4/5 cursor-pointer items-center rounded-lg  border-l-4 border-l-red-600 ${
         full ? 'bg-[#d1d2d3]' : 'bg-[#F8F9FA]'
-      } ${full ? '' : 'hover:bg-[#E9ECEF]'} p-1 drop-shadow-md`}
+      } ${full ? '' : 'hover:bg-[#E9ECEF]'}  drop-shadow-md ${
+        selected === hour ? 'bg-[#e3e6e9]' : ''
+      }`}
+      onClick={onClick}
     >
-      <div className="flex w-full">
-        <span className="flex-grow text-xl">{hour}</span>
+      <div className="flex h-full w-full items-center p-1">
+        <span className="flex h-full flex-grow items-center text-xl">
+          {hour}
+        </span>
         <span className="text-xl">{left}/20</span>
       </div>
     </div>
