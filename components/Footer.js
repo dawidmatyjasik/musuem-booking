79662597a865ab01fbff7899240797d178db164a
Dawid Spisak <dawidspisak@gmail.com>
2022-02-07 13:53:19 +0100

    fix responsivness

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex h-1/5 min-h-[180px] items-center justify-evenly bg-[#343A40] p-4">
+    <div className="flex h-1/5 min-h-[180px] flex-col items-center justify-evenly bg-[#343A40] p-4 sm:flex-row">
       <div className="flex flex-col space-y-2 text-white">
         <h3 className="text-xs">
           Muzeum Pamięci Mieszkańców Ziemi Oświęcimskiej
@@ -10,8 +10,12 @@ const Footer = () => {
         <h3 className="text-xs">ul. Maksymiliana Kolbego 2A,</h3>
         <h3 className="text-xs">32-600 Oświęcim</h3>
         <h3 className="text-xs">tel: 33 447 40 84</h3>
+        <h3 className="block text-xs sm:hidden">
+          {' '}
+          Copyright © 2022 | Created by Magme Agency
+        </h3>
       </div>
-      <h3 className="text-xs text-white">
+      <h3 className="hidden text-xs text-white sm:block">
         Copyright © 2022 | Created by Magme Agency{' '}
       </h3>
     </div>
