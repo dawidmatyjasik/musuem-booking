c936c7896b5ad9560430440812b924b0f3b7e8af
Dawid Spisak <dawidspisak@gmail.com>
2022-02-08 15:45:22 +0100

    conditional rendering

--- components/booking/HourCalendar.js
+++ components/booking/HourCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import { useRouter } from 'next/router'
 import HourTile from './HourTile'
@@ -15,6 +15,7 @@ const HourCalendar = () => {
       router.push(`/${router.query.date}/${selected.replace(':', '-')}`)
     }
   }
+
   /*   console.log(Object.keys(db[router.query.date].hours))
   Object.keys(db[router.query.date].hours).forEach((el, i) => {
     console.log(db[router.query.date].hours[el])
@@ -31,15 +32,19 @@ const HourCalendar = () => {
         <h1 className="text-2xl">Wybierz godzinę</h1>
       </div>
       <div className="my-4 flex max-h-[50vh] w-full flex-col  items-center space-y-2 overflow-x-hidden overflow-y-scroll scrollbar-hide">
-        {Object.keys(db[router.query.date].hours).map((el, i) => (
-          <HourTile
-            hour={el}
-            limit={db[router.query.date].hours[el].limit}
-            current={db[router.query.date].hours[el].current}
-            onClick={handleClick}
-            selected={selected}
-          />
-        ))}
+        {db[router.query.date] ? (
+          Object.keys(db[router.query.date].hours).map((el, i) => (
+            <HourTile
+              hour={el}
+              limit={db[router.query.date]?.hours[el].limit}
+              current={db[router.query.date]?.hours[el].current}
+              onClick={handleClick}
+              selected={selected}
+            />
+          ))
+        ) : (
+          <></>
+        )}
       </div>
       <button
         className="h-10 rounded border border-solid border-stone-600 px-8 py-1 inverted:border-yellow-400"
