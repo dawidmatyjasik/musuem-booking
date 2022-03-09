316d4131916d84a9c0008de78f76c54fd0b6820c
Dawid Spisak <dawidspisak@gmail.com>
2022-03-09 21:09:07 +0100

    change client rendering to server side

--- pages/[date]/[form].js
+++ pages/[date]/[form].js
@@ -2,14 +2,20 @@ import React from 'react'
 import FormComponent from '../../components/FormCalendar/FormComponent'
 import Layout from '../../components/Layout/Layout'
 
-const Hours = () => {
+const Hours = ({ data }) => {
   return (
     <div className="flex h-screen flex-col">
       <Layout>
-        <FormComponent />
+        <FormComponent data={data} />
       </Layout>
     </div>
   )
 }
 
 export default Hours
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/data')
+  const data = await response.json()
+  return { props: { data } }
+}
