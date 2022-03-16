baa80d4ed165a5d4640cbc983bea7149a716b509
Dawid Spisak <dawidspisak@gmail.com>
2022-03-16 21:51:50 +0100

    add authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,59 @@
 import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'dawid' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize(credentials, req) {
+        /*         const user = {
+          id: 1,
+          name: 'Dawid Spisak',
+          email: 'dawidspisak@example.com',
+        } */
+
+        if (
+          credentials.username === 'dawid' &&
+          credentials.password === 'test'
+        ) {
+          return {
+            id: 2,
+            name: 'Dawid',
+            email: 'dawid@gmail.com',
+          }
+        }
+
+        return null
+
+        /*         if (user) {
+          return user
+        } else {
+          return null
+        } */
+      },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id
+      }
+
+      return token
+    },
+    session: ({ token, session }) => {
+      if (token) {
+        session.id = token.id
+      }
+      return session
+    },
+  },
+  secret: 'test',
+  jwt: {
+    secret: 'test',
+  },
 })
--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Login = () => {
+  const { data: session } = useSession()
+  console.log(session)
+
   return (
     <div className="flex h-screen flex-col">
-      <Layout></Layout>
+      <Layout>
+        {session ? <h1>Zalogowany</h1> : <h1>Wylogowany</h1>}
+        {session ? (
+          <button onClick={() => signOut()}>Sign out</button>
+        ) : (
+          <button onClick={() => signIn()}>Sign in</button>
+        )}
+      </Layout>
     </div>
   )
 }
